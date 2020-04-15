d9242e03d498b4c7dc674c384eb17061947996d5
Puzanovim <puzanovim@yandex.ru>
2020-04-15 10:27:39 +0400

    add file Task

---
+++ src/Task.py
@@ -0,0 +1,16 @@
+def func(num1, num2):
+    return num1 + num2
+
+
+# str1 = input().split()
+# str2 = input().split()
+# N = int(str1[0])
+# S = int(str1[1])
+# speeds = [int(speed) for speed in str2]
+# times = [speed * 10 + speeds.index(speed) for speed in speeds]
+# shift = 0
+# for i in range(N):
+#     for j in range(N):
+#         if i < j and times[i] > times[j]:
+#             shift += 1
+# print(shift)
